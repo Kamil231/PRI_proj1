bb86a34d209ad2aa68a86ebeda7c86ac8791c4a0
Kamil231 <32738708+Kamil231@users.noreply.github.com>
2018-03-14 13:02:19 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <math.h>
+#include <limits>
+#include <stdlib.h>
+
+double potegowanie(double x, int n)
+{
+	if (n == 0)
+		return 1;
+
+	if (n % 2 == 1) 
+		return x * potegowanie(x, n - 1);
+
+	long long w = potegowanie(x, n / 2);
+	return w * w;
+
+}
+long long int silnia(int n)
+
+{
+	if (n == 0)
+		return 1;
+	else
+		return(n * silnia(n - 1));
+}
+int podaj_n(){
+	char *w, q[100];
+	int n = -1;
+	int i = 0;
+	printf("podaj n: \n");
+	while (n < 0){
+		while (fgets(q, sizeof(q), stdin)) {		// Wpisuje do q ciag znakow z klawiatury.
+			n = strtol(q, &w, 10);					// Do n przekazuje int z ciagu znakow q, do w reszte.
+			if (w == q || *w != '\n') {				// Jesli w ciagu znakow q nie bylo int to w == q, jesli pow wpisaniu inta wpisano cos innego niz enter to *w != '\n'
+				if (i > 0 )
+					printf("n musi byc calkowite, podaj n: \n", w, q);
+			}
+			else break;
+			i++;
+		}
+		if (n < 0)
+			printf("n musi byc dodatnie, podaj n: \n");
+	}
+	return n;
+}
+
+void main(){
+
+	long long int imax = std::numeric_limits<long long int>::max();	// maksymalna wartosc zmiennej typu long long int
+	double dmax = std::numeric_limits<double>::max();				// maksymalna wartosc zmiennej typu double
+
+	double x;						
+	printf("podaj x: \n");
+	scanf("%lf", &x);
+
+	int n;
+	n = podaj_n();
+	
+	double p;														// wartosc potegi
+	long long int s;												// wartosc silni
+	double coshx = 0;												// wartosc ciagu
+
+	for (int i = 0; i < n; i++){
+		p = potegowanie(x, 2 * i + 1);
+		s = silnia(2 * i + 1);
+		coshx = coshx + (1. / s) * p;								// suma pod kreska ulamkowa, za petla csch zostanie odwrocone
+		if (imax / (i + 1) < s || dmax / (2 * x) < p){				// sprawdza czy dla nastepnej petli nie zostanie przekrowczona max wartosc int lub double
+			printf("W nastepnej iteracji przekroczona zostalaby maksymalna wartosc int lub double, koniec obliczen dla n = %d\n", i);
+			break;
+		}
+	}
+
+	coshx = 1 / coshx;
+
+	double coshxr = 2 / (exp(x) - exp(-x));							// csch rzeczywiste
+	double blad = ((coshx - coshxr) / coshxr)*100;					// blad w procentach
+
+	printf("Wartosc cschx obliczona z ciagu: = %lf \nWartosc rzeczywista cschx = %lf \n", coshx, coshxr);
+	printf("blad = %lf %%\n", blad);
+
+}
